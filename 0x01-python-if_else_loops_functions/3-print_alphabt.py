fcf6462bf2aad16b79e8fa7115a77bc747ce1b08
kimberley-saka <kimberleysaka@gmail.com>
2022-12-04 09:55:58 -0800

    prints the alphabet except q and e

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""prints the ASCII alphabet, in lowercase, not followed by a new line
+except q and e"""
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
