e3630b6bbed8ae7b054c4dc5d2fb5d92c9b9f455
kimberley-saka <kimberleysaka@gmail.com>
2023-01-05 09:42:29 -0800

    printint integers only

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    i = 0
+    number = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            number += 1
+        except (ValueError, TypeError):
+            continue
+        print()
+        return number
