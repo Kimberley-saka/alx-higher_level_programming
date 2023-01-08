c52c09691ff00754e9ffeb91f0027f1ef2b99750
kimberley-saka <kimberleysaka@gmail.com>
2023-01-08 05:38:40 -0800

    incorrect output error fixed

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    for i in range (0, list_length):
+    i = 0
+    for i in range (list_length):
         try:
-            result = my_list_1 / my_list_2
+            result = my_list_1[i] / my_list_2[i]
         except TypeError:
             print("wrong type")
             result = 0
