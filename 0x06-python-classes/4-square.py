4e2d47898af516fee0df0d0be1486196d3cf0f06
kimberley-saka <kimberleysaka@gmail.com>
2023-01-08 08:18:54 -0800

    error fixed

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -23,7 +23,7 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.___size = value
+            self.__size = value
 
     def area(self):
         return self.__size * self.__size
