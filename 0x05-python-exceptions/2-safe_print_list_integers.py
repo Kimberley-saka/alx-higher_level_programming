28788096e0ca0373354b1cc10b296a4a4c880f23
kimberley-saka <kimberleysaka@gmail.com>
2023-01-08 03:50:47 -0800

    fixed error

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,5 +8,5 @@ def safe_print_list_integers(my_list=[], x=0):
             number += 1
         except (ValueError, TypeError):
             pass
-        print("")
-        return number
+    print("")
+    return number
