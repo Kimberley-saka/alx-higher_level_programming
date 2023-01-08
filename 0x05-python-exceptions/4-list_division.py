3e3450aad40322c2ff311b912774707dcb7972eb
kimberley-saka <kimberleysaka@gmail.com>
2023-01-08 05:40:14 -0800

    fixed for pycodestyle

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,7 +2,7 @@
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     i = 0
-    for i in range (list_length):
+    for i in range(list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
         except TypeError:
