60956fb0492d446a994d2576ad1f2801c5933e6f
kimberley-saka <kimberleysaka@gmail.com>
2023-02-14 04:02:07 -0800

    more sql

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Lists all records of the table second_table of the database hbtn_0c_0
+-- ordered by score (top first) i.e decending
+
+SELECT score, name FROM second_table ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,4 @@
+-- Lists all rows with a score >= 10 in the table secod_table
+-- In descending order
+
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
