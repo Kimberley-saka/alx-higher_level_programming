f21f7ec0a937ec516e700a5827cbbf26d0d8e70b
kimberley-saka <kimberleysaka@gmail.com>
2023-02-15 14:10:16 -0800

    fixed syntax error

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- Lists the priviledges of user_0d_1 and user_0d_2 in localhost
 
-SHOW GRANTS FOR user_0d_1@localhost
-SHOW GRANTS FOR user_0d_2@localhost
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
