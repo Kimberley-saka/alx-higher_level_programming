369dae5d691530da5ca61f57041b3b38f06da8d8
kimberley-saka <kimberleysaka@gmail.com>
2023-02-15 14:46:20 -0800

    fixed

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,3 @@
 -- creates new table with constraint
 
-CREATE TABLE IF NOT EXIXTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
