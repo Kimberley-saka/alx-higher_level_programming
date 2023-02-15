9c25169bc3e16fc1205b81e6f7305c7e1a9b0d5d
kimberley-saka <kimberleysaka@gmail.com>
2023-02-15 14:21:09 -0800

    create new database and user with select privileges

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- create new database and new user with SELECT privileges only
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_od_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON 'hbtn_0d_2' .* TO 'user_0d_2'@'localhost';
