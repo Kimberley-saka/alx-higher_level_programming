596954aabb3e60492566e4ab212afdacadd35376
Kimberley-saka <kimberleysaka@gmail.com>
2023-03-13 17:27:38 +0000

    fixed for semistandard

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const  multiLanguages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const multiLanguages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < 3; i++) {
   console.log(multiLanguages[i]);
