65245b939f139483a1b849b7143c3cec41d14728
Kimberley-saka <kimberleysaka@gmail.com>
2023-03-20 06:42:00 +0000

    further filter states table

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This script lists all states from the
+database `hbtn_0e_0_usa`.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    db_connect = MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306, password=argv[2], database=argv[3])
+
+    cursor = db_connect.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE NAME LIKE 'N%' /
+            ORDER BY state.id ASC")
+
+    rows_selected = cursor.fetchall()
+
+    for row in rows_selected:
+        print(row)
