9d38e60b7beff4833579c46c3da5f392c971d72e
Kimberley-saka <kimberleysak@gmail.com>
2023-04-03 03:30:31 -0700

    fixed

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Send a post request with data
-curl -sS -X POST -d "email=test@gmail&subject=I will always be here for PLD" "$1"
+curl -sS -X $1 POST -d "email=test@gmail&subject=I will always be here for PLD"
