da58a455e19eb6ec4371bd39c760f07197a662ed
Kimberley-saka <kimberleysak@gmail.com>
2023-04-04 02:06:37 -0700

    fixed a few things

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,9 +6,10 @@ import urllib.request
 """
 
 
-url_request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-with urllib.request.urlopen(url_request) as response:
-    body = response.read()
-
-print(f'Body response:$\n    - type: {type(body)}$\n    - content: {body}$')
-print(f'    - utf8 content: {body.decode("utf-8")}$')
+if __name__ == '__main__':
+    url_request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(url_request) as response:
+        body = response.read()
+    print(f'Body response:\n    - type: {type(body)}')
+    print(f'    - content: {body}')
+    print(f'    - utf8 content: {body.decode("utf-8")}')
