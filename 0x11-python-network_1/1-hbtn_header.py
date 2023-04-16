4872152b420542f1ca56d21dae79a3d5916b561c
Kimberley-saka <kimberleysak@gmail.com>
2023-04-16 06:04:35 -0700

    added documentation at the right place

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import sys
-import urllib.request
 """
 Displays the value of the X-Request-Id variable found
 in the header of the response
 """
+import sys
+import urllib.request
 
 
 if __name__ == "__main__":
