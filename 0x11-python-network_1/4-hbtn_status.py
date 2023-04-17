d288f977ddbdf0cc05d1646d950b959edf038248
Kimberley-saka <kimberleysak@gmail.com>
2023-04-17 04:19:55 -0700

    using requests package

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+fetches https://alx-intranet.hbtn.io/status
+"""
+import requests
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url)
+    print('Body response:')
+    print('\t- type:', type(response.content))
+    print('\t- content:', response.content.decode())
