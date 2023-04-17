db86cf6baa328ee8e56ee33b02edb41326a576f6
Kimberley-saka <kimberleysak@gmail.com>
2023-04-17 04:55:45 -0700

    fixed

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,8 +5,7 @@ fetches https://alx-intranet.hbtn.io/status
 import requests
 
 if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
-    response = requests.get(url)
-    print('Body response:')
-    print(f'\t- type:{type(response.text)}')
-    print(f'\t- content:{response.text}')
+    res = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
