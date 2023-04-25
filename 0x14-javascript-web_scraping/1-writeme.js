10712d6915ae595c9f0af8875f48839db634cb03
Kimberley-saka <kimberleysaka@gmail.com>
2023-04-25 09:34:36 +0000

    fixed for semistandard

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,11 +2,10 @@
 const fs = require('fs');
 
 const fileToWriteTo = process.argv[2];
-const data= process.argv[3];
+const data = process.argv[3];
 
 fs.writeFile(fileToWriteTo, data, 'utf-8', (error) => {
   if (error) {
     console.error(error);
-    return;
   }
 });
