07b9cb2f27ebab0de954c90ae3abd63a59fdbe92
Kimberley-saka <kimberleysaka@gmail.com>
2023-04-25 09:32:06 +0000

    write to file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const fileToWriteTo = process.argv[2];
+const data= process.argv[3];
+
+fs.writeFile(fileToWriteTo, data, 'utf-8', (error) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+});
