9a16bcd5d19bde0b6980c6ced046ff7823e49c2a
Kimberley-saka <kimberleysaka@gmail.com>
2023-04-25 13:52:42 +0000

    fixed for semistandard

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,10 +4,10 @@ const request = require('request');
 const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-request.get(url, (error,response, body) => {
-    if (error){
-        console.log(error);
-    }
-    const movieData = JSON.parse(body);
-    console.log(movieData.title);
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+  const movieData = JSON.parse(body);
+  console.log(movieData.title);
 });
