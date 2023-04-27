3bc806b2d679c7d18d7ccb77ff574e22552b16dc
Kimberley-saka <kimberleysaka@gmail.com>
2023-04-27 05:06:05 -0700

    fixed a trailing whitespace error

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
- takes your GitHub credentials (username and password) and 
+ takes your GitHub credentials (username and password) and
  uses the GitHub API to display your id
 """
 import requests
